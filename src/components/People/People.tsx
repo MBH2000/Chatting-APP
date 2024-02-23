c7c19920f3c6cca50fa969b8b6e6654f6fc58d89
MBH2000 <ava201466@gmail.com>
2024-02-22 18:52:23 -0800

    first commit

--- src/components/People/People.tsx
+++ src/components/People/People.tsx
@@ -1,8 +1,6 @@
-import { useAppSelector } from "@/Controllers/hooks"
+// import { useAppSelector } from "@/Controllers/hooks"
 import {
     Get_Info,
-    Add_Friend,
-    Remove_Friend
 } from './people-controllers'
 import { useLocation } from "react-router-dom"
 import { useEffect, useState } from "react";
@@ -15,16 +13,20 @@ export const People = () => {
     const [errorMessage, setErrorMessage] =useState('')  
 
 
-    const user=useAppSelector(state=>state.user)
+    // const user=useAppSelector(state=>state.user)
     async function Get_InfoFun(data:any){
         const respones=await Get_Info(data)
 
         if (respones && respones.error){
             setErrorMessage(respones.error);
+            console.log(errorMessage);
+
         }else{
             setInfo(respones)
             setLoading(true)
+            
         }
+        
     }
     useEffect(() => {
         setTimeout(()=>{
